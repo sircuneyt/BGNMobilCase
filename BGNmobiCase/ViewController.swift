c5cf423aa74109c3193cba4a4b33b7030914f7e9
Cüneyt ALSU <cuneytalsu@gmail.com>
2022-04-17 02:05:07 -0700

    Initial Commit

---
+++ BGNmobiCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BGNmobiCase
+//
+//  Created by Cüneyt ALSU on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
